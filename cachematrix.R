ebd235d401b7a7b7b6f698cdf2dac6cfd6416c6c
Kim D Holloway <47988443+kimh1000@users.noreply.github.com>
2019-03-18 09:51:07 -0500

    Update cachematrix.R
    Edited the original file with my code to calculate the inverse of a matrix if it doesn't already exist in a different environment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions are included in this file.  The first one assigns values to objects in an environment that is different from 
+## the current environment and passes those objects to the second function as a list.
 
-## Write a short comment describing this function
+## This first function creates the objects in a different environment from the current environment using the <<- assignment operator
 
 makeCacheMatrix <- function(x = matrix()) {
+    im <-  NULL
+  set <- function (y) {
+          x <<- y
+          im <<- NULL
+  }
+  get <-  function () x
+  setinv <-  function(invm) im <<- invm
+  getinv <- function() im
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
 
-}
 
-
-## Write a short comment describing this function
+## Second function below takes the output of makeCacheMatrix, checks to see if the inverse matrix exists and if not, calculates and
+## return its using the solve() funtion
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  im = x$getinv()
+  if (!is.null(im)) {
+    message("getting cached data")
+    return(im)
+  }
+  m.data <- x$get()
+  im <- solve(m.data, ...)
+  x$setinv(im)
+  return(im)
 }
